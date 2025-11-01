a6c05f25ee9b199965a5b8d420e0d2b80224272d
NataNatta <natalianikolak3@gmail.com>
2025-11-01 17:00:15 +0200

    Add initial React component for matrix calculator

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Калькулятор Матриці Долі</h1>
+      <p>Тут буде твій HTML-калькулятор</p>
+    </div>
+  );
+}
+
+export default App;
